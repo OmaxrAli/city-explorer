8f9139514247599b408c6235ec53947f735907c1
OmaxrAli <omar.abdelmajeed17@gmail.com>
2021-09-29 03:27:45 +0300

    lab 6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state={
+
+      locationResult: {}
+
+    }
+  }
+
+  location=()=>{
+
+    let URLlocation =`https://us1.locationiq.com/v1/search.php?key=pk.8d57889cc9cd17bdc8c659243ff3f0f8&q=amman&format=json`;
+
+
+   let locationResult = axios.get(URLlocation) ;
+    console.log(locationResult);
+    
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Location IQ</h2>
+        <button onClick={this.location}>Get location</button>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
+
